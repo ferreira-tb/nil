259a40f369bc86ade4b3c06b8390c425aef79640
Andrew Ferreira <andrew.shien2@gmail.com>
2025-12-04 21:18:18 -0300

    feat: add `spawn personnel` cheat (#148)
    * feat: add `spawn personnel` cheat
    
    * feat: add `spawn squad` cheat

---
+++ app/src/commands/cheat/military.ts
@@ -0,0 +1,35 @@
+// Copyright (C) Call of Nil contributors
+// SPDX-License-Identifier: AGPL-3.0-only
+
+import { invoke } from '@tauri-apps/api/core';
+import { CoordImpl } from '@/core/model/continent/coord';
+import { ArmyPersonnelImpl } from '@/core/model/military/army-personnel';
+
+export async function cheatSpawnPersonnel(
+  coord: ContinentKey,
+  personnel?: Option<ArmyPersonnel | number>,
+  ruler?: Option<Ruler>,
+) {
+  coord = CoordImpl.fromKey(coord);
+  personnel ??= 1_000;
+  ruler ??= null;
+
+  if (typeof personnel === 'number') {
+    personnel = ArmyPersonnelImpl.splat(personnel);
+  }
+
+  await invoke('cheat_spawn_personnel', { req: { coord, ruler, personnel } });
+}
+
+export async function cheatSpawnSquad(
+  coord: ContinentKey,
+  squad: Squad | UnitId,
+  ruler?: Option<Ruler>,
+) {
+  if (typeof squad === 'string') {
+    squad = { unit: squad, size: 1_000 };
+  }
+
+  const personnel = ArmyPersonnelImpl.fromSquad(squad);
+  await cheatSpawnPersonnel(coord, personnel, ruler);
+}
