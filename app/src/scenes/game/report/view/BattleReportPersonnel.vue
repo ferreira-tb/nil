2bb794c2423d5d78b8ed7d1265756a01e6e7dd22
Andrew Ferreira <andrew.shien2@gmail.com>
2025-12-09 15:27:26 -0300

    feat: improve `report-view` scene (#169)
    * feat: improve `report-view` scene
    
    * fix: remove label

---
+++ app/src/scenes/game/report/view/BattleReportPersonnel.vue
@@ -0,0 +1,56 @@
+<!-- Copyright (C) Call of Nil contributors -->
+<!-- SPDX-License-Identifier: AGPL-3.0-only -->
+
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n';
+import enUS from '@/locale/en-US/scenes/game/report.json';
+import ptBR from '@/locale/pt-BR/scenes/game/report.json';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@tb-dev/vue-components';
+
+const props = defineProps<{
+  personnel: ArmyPersonnel;
+  losses: ArmyPersonnel;
+}>();
+
+const { t } = useI18n({
+  messages: {
+    'en-US': enUS,
+    'pt-BR': ptBR,
+  },
+});
+</script>
+
+<template>
+  <Table>
+    <TableHeader>
+      <TableRow class="hover:bg-card">
+        <TableHead>{{ t('pikeman') }}</TableHead>
+        <TableHead>{{ t('swordsman') }}</TableHead>
+        <TableHead>{{ t('axeman') }}</TableHead>
+        <TableHead>{{ t('archer') }}</TableHead>
+        <TableHead>{{ t('light-cavalry') }}</TableHead>
+        <TableHead>{{ t('heavy-cavalry') }}</TableHead>
+      </TableRow>
+    </TableHeader>
+
+    <TableBody>
+      <TableRow class="hover:bg-card">
+        <TableCell>{{ personnel.pikeman.size }}</TableCell>
+        <TableCell>{{ personnel.swordsman.size }}</TableCell>
+        <TableCell>{{ personnel.axeman.size }}</TableCell>
+        <TableCell>{{ personnel.archer.size }}</TableCell>
+        <TableCell>{{ personnel.lightCavalry.size }}</TableCell>
+        <TableCell>{{ personnel.heavyCavalry.size }}</TableCell>
+      </TableRow>
+
+      <TableRow class="hover:bg-card">
+        <TableCell>{{ losses.pikeman.size }}</TableCell>
+        <TableCell>{{ losses.swordsman.size }}</TableCell>
+        <TableCell>{{ losses.axeman.size }}</TableCell>
+        <TableCell>{{ losses.archer.size }}</TableCell>
+        <TableCell>{{ losses.lightCavalry.size }}</TableCell>
+        <TableCell>{{ losses.heavyCavalry.size }}</TableCell>
+      </TableRow>
+    </TableBody>
+  </Table>
+</template>
---
+++ app/src/scenes/game/report/view/BattleReportTable.vue
@@ -0,0 +1,68 @@
+<!-- Copyright (C) Call of Nil contributors -->
+<!-- SPDX-License-Identifier: AGPL-3.0-only -->
+
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n';
+import { CoordImpl } from '@/core/model/continent/coord';
+import enUS from '@/locale/en-US/scenes/game/report.json';
+import ptBR from '@/locale/pt-BR/scenes/game/report.json';
+import { useCityOwnerSceneLink } from '@/composables/city/useCityOwnerSceneLink';
+import { useCityProfileSceneLink } from '@/composables/city/useCityProfileSceneLink';
+import BattleReportPersonnel from '@/scenes/game/report/view/BattleReportPersonnel.vue';
+import { Badge, Table, TableBody, TableCell, TableHead, TableRow } from '@tb-dev/vue-components';
+
+const props = defineProps<{
+  kind: BattleWinner;
+  winner: BattleWinner;
+  ruler: Ruler;
+  personnel: ArmyPersonnel;
+  losses: ArmyPersonnel;
+  city: PublicCity;
+}>();
+
+const { t } = useI18n({
+  messages: {
+    'en-US': enUS,
+    'pt-BR': ptBR,
+  },
+});
+
+const toRulerProfile = useCityOwnerSceneLink(() => props.ruler);
+const toCityProfile = useCityProfileSceneLink(() => props.city);
+</script>
+
+<template>
+  <Table class="max-w-[800px]">
+    <TableBody>
+      <TableRow>
+        <TableHead>{{ t(kind) }}</TableHead>
+        <TableCell>
+          <div class="flex items-center gap-4">
+            <RouterLink v-if="toRulerProfile" :to="toRulerProfile">
+              {{ ruler.id }}
+            </RouterLink>
+
+            <Badge v-if="kind === winner">
+              <span class="text-xs">{{ t('winner') }}</span>
+            </Badge>
+          </div>
+        </TableCell>
+      </TableRow>
+
+      <TableRow>
+        <TableHead>{{ kind === 'attacker' ? t('origin') : t('destination') }}</TableHead>
+        <TableCell>
+          <RouterLink v-if="toCityProfile" :to="toCityProfile">
+            {{ `${city.name} (${CoordImpl.format(city.coord)})` }}
+          </RouterLink>
+        </TableCell>
+      </TableRow>
+
+      <TableRow class="hover:bg-card">
+        <TableCell colspan="2">
+          <BattleReportPersonnel :personnel :losses />
+        </TableCell>
+      </TableRow>
+    </TableBody>
+  </Table>
+</template>
