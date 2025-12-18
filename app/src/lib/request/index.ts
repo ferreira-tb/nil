6169bd07e35c6fb26ae228add6d7d77bc626f6c5
Danilo Feitosa Soares <32103557+danilofs18@users.noreply.github.com>
2025-12-18 16:57:43 -0300

    feat: declared request interfaces in TypeScript (#204)

--- app/src/lib/request/index.ts
+++ app/src/lib/request/index.ts
@@ -1,10 +1,24 @@
 // Copyright (C) Call of Nil contributors
 // SPDX-License-Identifier: AGPL-3.0-only
 
-interface LeaveRequest {
+export type * from './npc';
+export type * from './chat';
+export type * from './city';
+export type * from './cheat';
+export type * from './round';
+export type * from './world';
+export type * from './battle';
+export type * from './player';
+export type * from './report';
+export type * from './ranking';
+export type * from './military';
+export type * from './continent';
+export type * from './infrastructure';
+
+export interface LeaveRequest {
   readonly world: WorldId;
 }
 
-interface WebsocketRequest {
+export interface WebsocketRequest {
   readonly world: WorldId;
 }
