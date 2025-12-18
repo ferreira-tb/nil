015e45f8cb9240cb3294b484c27a53ae935da3c2
Andrew Ferreira <andrew.shien2@gmail.com>
2025-12-18 15:48:56 -0300

    feat: add request types

---
+++ app/src/lib/request/index.ts
@@ -0,0 +1,10 @@
+// Copyright (C) Call of Nil contributors
+// SPDX-License-Identifier: AGPL-3.0-only
+
+interface LeaveRequest {
+  readonly world: WorldId;
+}
+
+interface WebsocketRequest {
+  readonly world: WorldId;
+}
